3cf66d7ded69eccea46b4fc4efebf81f5f596818
chuzui <chuqi77@gmail.com>
2017-05-12 09:48:55 +0800

    make scripts dir

--- script/symbol_link.sh
+++ script/symbol_link.sh
@@ -8,7 +8,7 @@
 
 dir=~/config                    # dotfiles directory
 olddir=~/config_old             # old dotfiles backup directory
-files="vimrc"    # list of files/folders to symlink in homedir
+files="vimrc zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
