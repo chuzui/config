d07849b3c34a81e13d52cde030caf0b2ef99de90
chuzui <chuqi77@gmail.com>
2017-05-17 22:04:12 +0800

    add install.sh

---
+++ script/install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+pip install jedi
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
