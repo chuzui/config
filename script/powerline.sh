3e0366019fefb23e61009d417d358530a81e1889
chuzui <chuqi77@gmail.com>
2017-05-18 15:11:24 +0800

    fix powerline.sh

--- script/powerline.sh
+++ script/powerline.sh
@@ -1,7 +1,7 @@
 pip install powerline-status
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.con
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
 mkdir -p ~/.fonts/ 
 mv PowerlineSymbols.otf ~/.fonts/
