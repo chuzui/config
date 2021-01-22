9398e9890ea817f461f59733158e24690d6c2245
chuzui <chuqi77@gmail.com>
2021-01-22 02:20:01 -0500

    Remove golang install

--- script/install.sh
+++ script/install.sh
@@ -12,7 +12,3 @@ pip install yapf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-sudo add-apt-repository ppa:gophers/archive
-sudo apt-get update
-sudo apt-get install golang-1.11-go
-
